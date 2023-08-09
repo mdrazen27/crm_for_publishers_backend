6c2af5e13875e3987d44b130a124bb1c87e1addd
Drazen Minic <mdrazen27@gmail.com>
2023-08-09 22:27:05 +0200

    Add relationships to Advertisement model and create factory

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Advertisement extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'url',
+        'status',
+        'publisher_id',
+    ];
+
+    protected $casts = [
+        'status' => 'boolean',
+    ];
+
+    public function publisher(): BelongsTo
+    {
+        return $this->belongsTo(Publisher::class);
+    }
+
+    public function statistics(): HasMany
+    {
+        return $this->hasMany(Statistic::class);
+    }
 }
