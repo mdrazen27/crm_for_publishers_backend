10ab6ccb88b6f13660df06282fd9ba8e60885870
Drazen Minic <mdrazen27@gmail.com>
2023-08-09 18:01:24 +0200

    Alter user, remove unnecessary fields, and make seeder for admins

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'role_id' => 1
         ];
     }
 
@@ -34,5 +33,15 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function admin(): static
+    {
+        return $this->state(['role_id' => 1]);
+    }
+
+    public function publisher(): static
+    {
+        return $this->state(['role_id' => 2]);
     }
 }
